37dfc60eeaa7858da8ca1df0c0937c0f991d75a1
Jamie Epstein <jasepstein@gmail.com>
2021-04-13 20:55:10 -0500

    Base app with routes roughed in

---
+++ bglibrary/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Route path="/" exact component={Home} />
+        <Route path ="/profile" component={Profile} />
+      </>
+      
+    )
+  }
+}
+
+export default App;
