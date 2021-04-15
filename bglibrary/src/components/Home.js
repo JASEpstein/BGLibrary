ae2bf42e25a394a5388e6d2f657bb037a2aaf906
Jamie Epstein <jasepstein@gmail.com>
2021-04-15 08:43:54 -0500

    Login fueature working properly

--- bglibrary/src/components/Home.js
+++ bglibrary/src/components/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     render() {
+        //Shortens the method calls later in the file
+        const { isAuthenticated, login } = this.props.auth;
+
+        //Conditionally display Login button or Profile link if authenticated
         return (
             <div>
                 <h2>HOME</h2>
-                <button onClick={this.props.auth.login}>Login</button>
+                {isAuthenticated() ? (
+                    <Link to='/profile'>View Profile</Link>
+                ) : (
+                    <button onClick={login}>Login</button>
+                )}
             </div>
         );
     }
