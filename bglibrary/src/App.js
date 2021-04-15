44b19a1a4600c670cc68d75ba47af11da3c80bb3
Jamie Epstein <jasepstein@gmail.com>
2021-04-15 08:13:06 -0500

    Storing JWT in localStorage oh nono

--- bglibrary/src/App.js
+++ bglibrary/src/App.js
@@ -4,19 +4,21 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import Nav from './components/Nav';
 import Auth from './components/Auth'
+import Callback from './components/Callback';
 class App extends Component {
   constructor(props){
     super(props)
     this.auth = new Auth(this.props.history);
-
   }
+
   render() {
     return (
       <>
         < Nav />
         <div className='body'>
           <Route path="/" exact render={ props => <Home auth={this.auth} {...props} />} />
-          <Route path ="/profile" component={Profile} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/callback" exact render={ props => <Callback auth={this.auth} {...props} />} />
         </div>
       </>
       
